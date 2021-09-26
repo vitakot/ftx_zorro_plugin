75fd8353d1cd8273707203a865dd32cf4f4c7af7
Vitezslav Kot <vitezslav.kot@gmail.com>
2021-09-26 15:53:13 +0200

    Added cpphttplib

--- include/ftx_client.h
+++ include/ftx_client.h
@@ -10,9 +10,18 @@ Copyright (c) 2021 Vitezslav Kot <vitezslav.kot@gmail.com>.
 #ifndef FTX_ZORRO_PLUGIN_FTXCLIENT_H
 #define FTX_ZORRO_PLUGIN_FTXCLIENT_H
 
+#include <string>
+
 class FTXClient {
 
+  //  util::HTTPSession http_client;
+    const std::string uri = "ftx.com";
+    const std::string api_key = "";
+    const std::string api_secret = "";
+    const std::string subaccount_name = "";
+
 public:
+
     void getAccountInfo();
 
 };
